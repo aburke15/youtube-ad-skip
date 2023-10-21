5933e206b198ec88d44f3562499620fffed088ab
Andre Burke <aburke.dev@gmail.com>
2023-10-21 17:10:33 -0600

    initial commit

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,92 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+setInterval(() => {
+  if (isSkippableAd()) {
+    skipAd();
+  } else if (isAdOverlay() && !isMuted()) {
+    toggleMute();
+  } else if (!isAdOverlay() && isMuted()) {
+    toggleMute();
+  }
+}, 550);
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+function toggleMute() {
+  const muteBtn = getMuteBtnIfExists();
+
+  if (muteBtn !== null) {
+    muteBtn.click();
+  }
+}
+
+function skipAd() {
+  const skipAdBtn = getSkipAdBtnIfExists();
+
+  if (skipAdBtn !== null) {
+    skipAdBtn.click();
+  }
+}
+
+function isSkippableAd() {
+  const skipAdBtn = getSkipAdBtnIfExists();
+  return skipAdBtn !== null;
+}
+
+function isMuted() {
+  const muteBtn = getMuteBtnIfExists();
+
+  if (muteBtn !== null) {
+    const attrValue = muteBtn.getAttribute("data-title-no-tooltip");
+    if (attrValue && attrValue.toUpperCase() === "UNMUTE") {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function isAdOverlay() {
+  const unskippableAd = document.getElementsByClassName("ytp-ad-player-overlay");
+
+  if (unskippableAd && unskippableAd.length > 0) {
+    return true;
+  }
+
+  return false;
+}
+
+function getSkipAdBtnIfExists() {
+  const skipAdElements = document.getElementsByClassName("ytp-ad-skip-button");
+
+  if (skipAdElements && skipAdElements.length > 0) {
+    return skipAdElements[0];
+  }
+
+  return null;
+}
+
+function getMuteBtnIfExists() {
+  const muteElements = document.getElementsByClassName("ytp-mute-button");
+
+  if (muteElements && muteElements.length > 0) {
+    return muteElements[0];
+  }
+
+  return null;
+}
+
+function unmuteAfterAd() {
+  const currentDurationElements = document.getElementsByClassName("ytp-time-current");
+  const totalDurationElements = document.getElementsByClassName("ytp-time-duration");
+
+  if (!currentDurationElements) return;
+  if (!totalDurationElements) return;
+  if (currentDurationElements.length < 1) return;
+  if (totalDurationElements.length < 1) return;
+
+  const currentDuration = currentDurationElements[0];
+  const totalDuration = totalDurationElements[0];
+  const currentDurationText = currentDuration.textContent;
+  const totalDurationText = totalDuration.textContent;
+
+  if (currentDurationText === totalDurationText) {
+    toggleMute();
+  }
+}
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,65 @@
-console.log("Hello World!", browser);
+function test() {
+  console.log("test!");
+}
+
+function toggleNormal() {
+  disableOtherMenuItem();
+  // const menuElement = getMenuItemElement("NORMAL");
+  // toggleSpeed(menuElement);
+}
+
+function toggleOnePointSevenFive() {
+  disableOtherMenuItem();
+  const menuElement = getMenuItemElement("1.75");
+  toggleSpeed(menuElement);
+}
+
+function toggleDoubleSpeed() {
+  disableOtherMenuItem();
+  const menuElement = getMenuItemElement("2");
+  toggleSpeed(menuElement);
+}
+
+function toggleSpeed(menuElement) {
+  if (menuElement) {
+    menuElement.setAttribute("aria-checked", "true");
+  }
+}
+
+function disableOtherMenuItem() {
+  const menuElements = document.getElementsByClassName("ytp-menuitem-label");
+
+  console.log(menuElements);
+
+  if (!menuElements || menuElements.length < 1) {
+    return;
+  }
+
+  let toggledElement = null;
+  for (let menuElement of menuElements) {
+    if (!menuElement) return;
+
+    toggledElement = menuElement.parentElement;
+    const attrValue = toggledElement.getAttribute("aria-checked");
+
+    if (attrValue === "true" || attrValue) {
+      toggledElement.removeAttribute("aria-checked");
+      return;
+    }
+  }
+}
+
+function getMenuItemElement(innerText) {
+  const menuElements = document.getElementsByClassName("ytp-menuitem-label");
+
+  let parentElement = null;
+  if (menuElements && menuElements.length > 0) {
+    for (let menuElement of menuElements) {
+      if (menuElement && menuElement.textContent.toUpperCase() === innerText) {
+        parentElement = menuElement.parentElement;
+      }
+    }
+  }
+
+  return parentElement;
+}
